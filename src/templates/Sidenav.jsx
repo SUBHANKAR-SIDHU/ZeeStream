3ec55079ef04cd4da59a8d939699ef91bb566439
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-02 13:00:09 +0530

    write link as a key in sideNav object

--- src/templates/Sidenav.jsx
+++ src/templates/Sidenav.jsx
@@ -5,23 +5,28 @@ function Sidenav() {
     const sideNav = [
         {
             title: "Trending",
-            icon : <i className="ri-fire-line"></i>
+            icon : <i className="ri-fire-line"></i>,
+            link: "/trending"
         },
         {
             title: "Popular",
-            icon : <i className="ri-bard-line"></i>
+            icon : <i className="ri-bard-line"></i>,
+            link:"/popular"
         },
         {
             title: "Movies",
-            icon : <i className="ri-video-line"></i>
+            icon : <i className="ri-video-line"></i>,
+            link:"/movies"
         },
         {
             title: "Tv Shows",
-            icon : <i className="ri-tv-line"></i>
+            icon : <i className="ri-tv-line"></i>,
+            link:"tvshows"
         },
         {
             title: "People",
-            icon : <i className="ri-team-line"></i>
+            icon : <i className="ri-team-line"></i>,
+            link: "/people"
         },
     ]
     return (
@@ -30,7 +35,7 @@ function Sidenav() {
             <nav>
                 <h1 className='font-bold  mt-5 mb-3'>New Feeds</h1>
                 {sideNav.map((s,i)=>(
-                    <Link key={i} className='font-semibold  flex gap-1 mb-3 w-full px-4 py-2  hover:border-none hover:bg-[#6556CD] rounded duration-300'>{s.icon}{s.title}</Link>
+                    <Link to={s.link} key={i} className='font-semibold  flex gap-1 mb-3 w-full px-4 py-2  hover:border-none hover:bg-[#6556CD] rounded duration-300'>{s.icon}{s.title}</Link>
                 ))}
                 <hr />
                 <Link className='font-semibold mt-4  flex gap-1 mb-3 w-full px-4 py-2  hover:border-none hover:bg-[#6556CD] rounded duration-300'><i className="ri-info-card-line"></i>About <span className='text-red-400'>Stream</span></Link>
