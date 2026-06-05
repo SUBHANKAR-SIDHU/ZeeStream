4767e3ff312bf1c03bab23ad31c94284c3c9e57d
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-05 12:04:49 +0530

    add popular,movie and about route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Trending from './templates/Trending'
+import Popular from './templates/Popular'
+import Movie from './templates/Movie'
+import About from './components/About'
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <div className='w-screen h-screen bg-[#1F1E24] flex'>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Trending" element={<Trending />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/movies" element={<Movie />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </>
