1e30fd8b469b60463e00ed51a6a3cd33d6813427
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-08 20:32:25 +0530

    create store and actions,ruducer,store.jsx

---
+++ src/store/store.jsx
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { movieSlice } from "./reducer/movieSlicer";
+import { peopleSlice } from "./reducer/peopleSlicer";
+import { tvSlice } from "./reducer/tvSlicer";
+
+export const store = configureStore({
+    reducer: {
+        movie: movieSlice,
+        tv: tvSlice,
+        people: peopleSlice,
+    },
+})
