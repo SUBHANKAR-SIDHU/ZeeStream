27230e9371c17aafbf08d85e6d7a11aff6009297
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-09 23:07:04 +0530

    creating action for movie details

---
+++ src/store/actions/movieActions.jsx
@@ -0,0 +1,25 @@
+export { removemovie } from "../reducer/movieSlicer";
+import axios from "../../utils/axios";
+import { loadmovie } from "../reducer/movieSlicer";
+
+
+export const asyncloadmovie = (id) => async (dispatch, getState) => {
+    try {
+        const detail = await axios.get(`/movie/${id}`);
+        const externalid = await axios.get(`/movie/${id}/external_ids`);
+        const recommendation = await axios.get(`/movie/${id}/recommendations`);
+        const similar = await axios.get(`/movie/${id}/similar`);
+        const video = await axios.get(`/movie/${id}/videos`);
+        const watchprovider = await axios.get(`/movie/${id}/watch/providers`);
+        let allDetails = {
+            detail: detail.data,
+            externalid: externalid.data,
+            recommendation: recommendation.data.results,
+            similar : similar.data,
+            video : video.data ,
+            watchprovider : watchprovider.data
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
